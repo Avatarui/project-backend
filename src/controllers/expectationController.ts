af60ead1bea0819be4c7a93c7fb8967a66e0a915
Avatarui1 <65011212186@msu.ac.th>
2025-09-27 14:35:44 +0700

    configthis

--- src/controllers/expectationController.ts
+++ src/controllers/expectationController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import {
   createExpectationService,
   getExpectationsByUserService,
+  checkExpectationDB,
+  getExpectationsByActIdAndUser
 } from "../services/expectationService";
 import { ExpectationBody } from "../types/expectation";
 
@@ -28,5 +30,25 @@ export const getExpectationsByUser = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Error fetching expectations:", error);
     res.status(500).json({ message: "Database error" });
+  }
+};
+export const checkExpectationByActId = async (req: Request, res: Response) => {
+  const { act_id, uid } = req.body;
+
+  if (!act_id || !uid) return res.status(400).json({ message: "act_id and uid are required" });
+
+  try {
+    const exists = await checkExpectationDB(act_id, uid); // ปรับ service ให้รับ uid ด้วย
+    let userExp = null;
+
+    if (exists) {
+      const expectations = await getExpectationsByActIdAndUser(act_id, uid);
+      userExp = expectations.user_exp;
+    }
+
+    return res.status(200).json({ exists, user_exp: userExp });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Database error" });
   }
 };
--- src/routes/activity.ts
+++ src/routes/activity.ts
@@ -7,14 +7,27 @@ import {
   updateActivity,
 } from "../controllers/activityController";
 import { authenticateToken } from "../middlewares/auth";
+import path from "path";
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads")); // โฟลเดอร์ uploads
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+
 const upload = multer({
-  storage: multer.memoryStorage(), // เก็บไฟล์ใน RAM ชั่วคราว
-  limits: { fileSize: 5 * 1024 * 1024 }, // ขนาดสูงสุด 5MB
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
 const router = Router();
 
 router.get("/getAct", authenticateToken,getActivities);
-router.post("/createAct", upload.single("cateImage"), createActivity);
-router.put("/updateAct", updateActivity);
+router.post("/createAct", authenticateToken, createActivity);
+// router.post("/createAct", upload.single("act_pic"), createActivity);
+router.put("/updateAct", upload.single("act_pic"),updateActivity);
 router.post("/deleteAct", deleteActivity);
 export default router;
