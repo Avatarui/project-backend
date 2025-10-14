54cf6938581f77a6a3855b9b6c795097c57fd0d5
Avatarui1 <65011212186@msu.ac.th>
2025-10-15 00:38:43 +0700

    firebaseEdit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,24 @@ import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const serviceAccount = require("../finalproject-609a4-firebase-adminsdk-fbsvc-e4975b201d.json");
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key?.replace(/\\n/g, "\n"),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+};
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    // storageBucket: process.env.FIREBASE_BUCKET,
-  });
-  
-  export const auth = admin.auth();
-  export const db = admin.firestore();
-  export const bucket = admin.storage().bucket();
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  // storageBucket: process.env.FIREBASE_BUCKET,
+});
+
+export const auth = admin.auth();
+export const db = admin.firestore();
+export const bucket = admin.storage().bucket();
